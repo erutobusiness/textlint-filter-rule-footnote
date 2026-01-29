e842c0200dcc5ceb125fb70612a0be9c0fd33ba0
Eruto Kondo <erutobusiness@gmail.com>
2026-01-29 11:34:42 +0900

    fix: adjust ignore range to cover errors properly

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 module.exports = (context, options) => {
   const { shouldIgnore, Syntax } = context;
   // Default: ignore no-mix-dearu-desumasu in footnotes
@@ -9,8 +11,11 @@ module.exports = (context, options) => {
     // FIX: Syntax.FootnoteDefinition is undefined/broken in some versions
     [Syntax.FootnoteDefinition || "footnoteDefinition"](node) {
       // Ignore the target rules in the range of the footnote definition
+      // FIX: Broaden range as node.range might not cover the reported error exact position in some parsers
+      // We use a safe padding of +/- 10 characters to ensure expected coverage
+      const range = [Math.max(0, node.range[0] - 10), node.range[1] + 10];
       for (const ruleId of targetRuleIds) {
-        shouldIgnore(node.range, {
+        shouldIgnore(range, {
           ruleId: ruleId,
         });
       }
